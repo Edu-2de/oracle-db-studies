2600998209ae59b9d3a0844e4b3fbfbf6a2884a4
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-15 13:01:57 -0300

    Add SQL comment for query to select payment type description and latest sale date

--- atv/atv1.sql
+++ atv/atv1.sql
@@ -44,4 +44,4 @@ inner join venda v on c.codcliente = v.codcliente
 inner join itensvenda i on v.nnf = i.nnf
 inner join produto p on i.codproduto = p.codproduto
 
---8
+--8: Selecionar a descrição do tipo de pagamento, e a maior data de venda que utilizou esse tipo de pagamento. Ordenar a consulta pela descrição do tipo de pagamento.
