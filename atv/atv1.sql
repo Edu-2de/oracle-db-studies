8c861150992dba30cac29b843f5f8b2bce943813
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-14 22:28:40 -0300

    Add initial SQL script for client and product purchase queries

---
+++ atv/atv1.sql
@@ -0,0 +1,12 @@
+// cSpell:disable
+
+--1: Selecionar o nome do cliente e quantidade de produtos comprados, somente para
+clientes que compraram Coca Cola
+select c.cliente, i.qtde
+from cliente c
+inner join venda v on c.codcliente = v.codcliente
+inner join itensvenda i on v.nnf = i.nnf
+inner join produto p on i.codproduto = p.codproduto
+where p.descricaoproduto = 'Coca Cola';
+
+--2: Selecionar o nome do cliente e o valor total comprado por ele.
