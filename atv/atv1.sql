64ce1a3a8e181cf63501ecf9630faf2c1d8c8ac4
Eduardo Da Silva Paim <edupaim1712@gmail.com>
2025-08-15 20:12:51 -0300

    Update atv1.sql

--- atv/atv1.sql
+++ atv/atv1.sql
@@ -9,11 +9,10 @@ inner join produto p on i.codproduto = p.codproduto
 where p.descricaoproduto = 'Coca Cola'
 
 --2: Selecionar o nome do cliente e o valor total comprado por ele.
-select c.cliente, sum(i.qtde * p.preco) as sum
+select c.cliente, sum(v.vlvenda) as sum
 from cliente c
 inner join venda v on c.codcliente = v.codcliente
-inner join itensvenda i on v.nnf = i.nnf
-inner join produto p on i.codproduto = p.codproduto
+GROUP BY c.cliente
 
 --3: Selecionar a descrição e o maior preço de produto vendido.
 select p.descricaoproduto, max(p.preco) from produto p
