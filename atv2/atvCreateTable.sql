3b1a8c1ee4055a845085ce90be42c18f98c112c0
EDUARDO DA SILVA PAIM <85540781015@senacrs.edu.br>
2025-08-20 21:13:16 -0300

    feat: refresh atv create table

--- atv2/atvCreateTable.sql
+++ atv2/atvCreateTable.sql
@@ -1,43 +1,10 @@
-ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY';
-
-CREATE TABLE produto (
-  cod_prod INTEGER NOT NULL,
-  nome VARCHAR2(30) NOT NULL,
-  preco NUMBER NOT NULL,
-  categoria VARCHAR2(20) NOT NULL,
-  PRIMARY KEY (cod_prod)
-);
-
-CREATE TABLE cliente (
-  cod_cli CHAR(10) NOT NULL,
-  nome VARCHAR2(40) NOT NULL,
-  cidade VARCHAR2(20) NOT NULL,
-  uf CHAR(2) NOT NULL,
-  telefone VARCHAR2(20),
-  status CHAR(5) NOT NULL,
-  limite NUMBER NOT NULL,
-  PRIMARY KEY (cod_cli)
-);
-
-CREATE TABLE pedido (
-  nro_ped CHAR(10) NOT NULL,
-  data_elab DATE NOT NULL,
-  data_ent DATE,
-  cod_cli CHAR(10) NOT NULL,
-  PRIMARY KEY (nro_ped),
-  FOREIGN KEY (cod_cli) REFERENCES cliente(cod_cli)
-);
-
-CREATE TABLE movimento (
-  nro_ped CHAR(10) NOT NULL,
-  cod_prod INTEGER NOT NULL,
-  qtde INTEGER NOT NULL,
-  total_mov NUMBER NOT NULL,
-  PRIMARY KEY (nro_ped, cod_prod),
-  FOREIGN KEY (nro_ped) REFERENCES pedido(nro_ped),
-  FOREIGN KEY (cod_prod) REFERENCES produto(cod_prod)
-);
-
-ALTER TABLE pedido ADD total_ped NUMBER;
-
-CREATE INDEX Ch_Pr_Mov ON movimento (qtde);
+create table curso (codcurso int PRIMARY KEY, curso varchar(50), duracao int, dtcurso
+date);
+create table analista (codanalista int PRIMARY KEY, analista varchar(50), idade int, endereco
+char(30),codcurso int, salario float);
+create table programador (codprogramador int PRIMARY KEY, progranador varchar(50),
+idade int, endereco char(30), salario float);
+create table atividadesanalise (codatividadeanalise int PRIMARY KEY, dtinicio
+date,dttermino date, descricao varchar(100), codanalista int);
+create table atividadesprog (codatividadeprog int PRIMARY KEY, dtinicio date,dttermino
+date, descricao varchar(100), codprogramador int, codatividadeanalise int);
