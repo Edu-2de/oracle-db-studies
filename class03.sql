38476139dfcb0cc5b66688d8ff808d1675e01399
Eduardo Da Silva Paim <edupaim1712@gmail.com>
2025-08-20 21:04:39 -0300

    Create class03.sql

---
+++ class03.sql
@@ -0,0 +1,13 @@
+SELECT * FROM cliente
+
+DROP VIEW vw_cliente
+
+CREATE OR REPLACE VIEW vw_cliente AS
+SELECT cod_cli, nome, cidade, uf, status FROM cliente
+where cidade = 'Canoas'
+
+SELECT * FROM vw_cliente c, pedido p
+where c.cod_cli = p.cod_cli
+
+INSERT INTO cliente(cod_cli, nome, cidade, uf, status, limite) VALUES
+('c5', 'SEFORA', 'Caxias', 'RS', 'bom', 500)
