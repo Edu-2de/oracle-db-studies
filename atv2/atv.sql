734ad6bf135978f44f73ae39f821eb0dee4e1129
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-22 14:29:34 -0300

    fix: correct join condition in vw_cursosanalista view

--- atv2/atv.sql
+++ atv2/atv.sql
@@ -2,13 +2,13 @@
 --1: Criar uma view com o nome de cursosanalista, que contém o nome do curso, nome do analista e salário do analista com um aumento de 10%.
 
 CREATE OR REPLACE VIEW vw_cursosanalista AS
-SELECT Analista, salario, Codcurso FROM analista;
+SELECT Analista, salario, Codcurso FROM analista
 
 UPDATE analista
 SET salario = salario * 1.10;
 
 SELECT c.Curso, a.Analista, a.Salario FROM vw_cursosanalista a
-JOIN curso c ON a.Codcurso = c.Codcurso;
+JOIN curso c ON a.Codcurso = c.Codcursol;
 
 --2: Montar uma consulta que mostra o nome do programador e a quantidade de dias de férias. Caso o programador tenha idade
 --de 20 a 24 anos 18 dias
