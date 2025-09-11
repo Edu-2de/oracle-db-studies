bdce04b974ae1192a7174a291a4317919cb5dbcd
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-11 10:24:43 -0300

    Fix SQL syntax by adding missing semicolons in multiple queries in atv.sql

--- atv2/atv.sql
+++ atv2/atv.sql
@@ -7,23 +7,23 @@ from cliente c
 inner join venda v on c.codcliente = v.codcliente
 inner join itensvenda i on v.nnf = i.nnf and v.dtvenda = i.dtvenda
 inner join produto p on i.codproduto = p.codproduto
-where p.descricaoproduto = 'Coca Cola'
+where p.descricaoproduto = 'Coca Cola';
 
 --2: Selecionar o nome do cliente e o valor total comprado por ele.
 select c.cliente, sum(v.vlvenda) as sum
 from cliente c
 inner join venda v on c.codcliente = v.codcliente
-GROUP BY c.cliente
+GROUP BY c.cliente;
 
 --3: Selecionar a descrição e o maior preço de produto vendido.
 select p.descricaoproduto, max(p.preco) from produto p
-GROUP BY p.descricaoproduto
+GROUP BY p.descricaoproduto;
 
 --4: Selecionar o nome do cliente e descrição do tipo de pagamento utilizado nas vendas.
 select c.cliente, t.descricaotppagamento
 from cliente c
 inner join venda v on v.codcliente = c.codcliente
-inner join tipospagamento t on v.codtppagamento = t.codtppagamento
+inner join tipospagamento t on v.codtppagamento = t.codtppagamento;
 
 --5: Selecionar o nome do cliente, nnf, data da venda, descrição do tipo de pagamento,descrição do produto e quantidade vendida dos itens vendidos.
 select c.cliente, i.nnf, i.dtvenda, t.descricaotppagamento, p.descricaoproduto, i.qtde
@@ -31,7 +31,7 @@ from cliente c
 inner join venda v on c.codcliente = v.codcliente
 inner join tipospagamento t on v.codtppagamento = t.codtppagamento
 inner join itensvenda i on v.nnf = i.nnf
-inner join produto p on i.codproduto = p.codproduto
+inner join produto p on i.codproduto = p.codproduto;
 
 --6: Selecionar a média de preço dos produtos vendidos
 SELECT ROUND(SUM(i.qtde * p.preco) / SUM(i.qtde), 3)
@@ -43,20 +43,20 @@ select distinct c.cliente, p.descricaoproduto
 from cliente c
 inner join venda v on c.codcliente = v.codcliente
 inner join itensvenda i on v.nnf = i.nnf and v.dtvenda = i.dtvenda
-inner join produto p on i.codproduto = p.codproduto
+inner join produto p on i.codproduto = p.codproduto;
 
 --8: Selecionar a descrição do tipo de pagamento, e a maior data de venda que utilizou esse tipo de pagamento. Ordenar a consulta pela descrição do tipo de pagamento.
 select t.descricaotppagamento, max(v.dtvenda)
 from venda v
 inner join tipospagamento t on v.codtppagamento = t.codtppagamento
-group by t.descricaotppagamento
+group by t.descricaotppagamento;
 
 --9: Selecionar a data da venda e a média da quantidade de produtos vendidos. Ordenar pela data da venda decrescente
 select v.dtvenda, avg(i.qtde)
 from venda v 
 inner join itensvenda i on v.nnf = i.nnf and v.dtvenda = i.dtvenda
 group by v.dtvenda
-order by v.dtvenda desc
+order by v.dtvenda desc;
 
 --10: Selecionar a descrição do produto e a média de quantidades vendidas do produto. Somente se a média for superior a 4
 SELECT
